c31976784cb513b318e7998918bec852a0cce701
rahul4287452 <rahulgarud122@gmail.com>
2025-09-20 00:44:46 +0530

    Removed DB, models, forms and views

--- bugtracker/bugs/forms.py
+++ bugtracker/bugs/forms.py
@@ -1,18 +1,18 @@
-from django import forms
-from .models import Bug
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+# from django import forms
+# from .models import Bug
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.models import User
 
-class BugForm(forms.ModelForm):
-    class Meta:
-        model = Bug
-        fields = ("title","description","status","priority","screenshot","assigned_to")
+# class BugForm(forms.ModelForm):
+#     class Meta:
+#         model = Bug
+#         fields = ("title","description","status","priority","screenshot","assigned_to")
 
-class UserRegistrationForm(UserCreationForm):
-    email = forms.EmailField()
-    class Meta:
-        model = User
-        fields = ("username","email","password1","password2")
+# class UserRegistrationForm(UserCreationForm):
+#     email = forms.EmailField()
+#     class Meta:
+#         model = User
+#         fields = ("username","email","password1","password2")
 
 
     
